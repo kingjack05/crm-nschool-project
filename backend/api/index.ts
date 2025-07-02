c85c0409a4f47bad6123922d7505ee709233b677
kingjack05 <kingjack05@gmail.com>
2025-07-02 20:53:59 +0800

    init

---
+++ backend/api/index.ts
@@ -0,0 +1,48 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { RPCHandler } from "@orpc/server/fetch";
+import { OpenAPIHandler } from "@orpc/openapi/fetch";
+import { router } from "../router/index.js";
+
+export const config = {
+  runtime: "edge",
+};
+
+const app = new Hono().basePath("/api");
+
+const rpcHandler = new RPCHandler(router);
+
+app.use("/rpc/*", async (c, next) => {
+  const { matched, response } = await rpcHandler.handle(c.req.raw, {
+    prefix: "/api/rpc",
+    context: {}, // Provide initial context if needed
+  });
+
+  if (matched) {
+    return c.newResponse(response.body, response);
+  }
+
+  await next();
+});
+
+const openapiHandler = new OpenAPIHandler(router);
+
+app.use("/rest/*", async (c, next) => {
+  console.log(c.req.url);
+  const { matched, response } = await openapiHandler.handle(c.req.raw, {
+    prefix: "/api/rest",
+    context: {}, // Provide initial context if needed
+  });
+  console.log("matched", matched);
+  if (matched) {
+    return c.newResponse(response.body, response);
+  }
+
+  await next();
+});
+
+app.get("/", (c) => {
+  return c.json({ message: "Hello Hono!" });
+});
+
+export default handle(app);
---
+++ backend/router/index.ts
@@ -0,0 +1,56 @@
+import { ORPCError, os } from "@orpc/server";
+import { z } from "zod";
+
+const PlanetSchema = z.object({
+  id: z.number().int().min(1),
+  name: z.string(),
+  description: z.string().optional(),
+});
+
+export const listPlanet = os
+  .route({ method: "GET", path: "/planets" })
+  .input(
+    z.object({
+      limit: z.number().int().min(1).max(100).optional(),
+      cursor: z.number().int().min(0).default(0),
+    })
+  )
+  .output(z.array(PlanetSchema))
+  .handler(async ({ input }) => {
+    // your list code here
+    return [{ id: 1, name: "name" }];
+  });
+
+export const findPlanet = os
+  .route({ method: "GET", path: "/planets/{id}" })
+  .input(PlanetSchema.pick({ id: true }))
+  .output(PlanetSchema)
+  .handler(async ({ input }) => {
+    // your find code here
+    return { id: 1, name: "name" };
+  });
+
+// export const createPlanet = os
+//   .$context<{ headers: IncomingHttpHeaders }>()
+//   .use(({ context, next }) => {
+//     const user = parseJWT(context.headers.authorization?.split(' ')[1])
+
+//     if (user) {
+//       return next({ context: { user } })
+//     }
+
+//     throw new ORPCError('UNAUTHORIZED')
+//   })
+//   .input(PlanetSchema.omit({ id: true }))
+//   .handler(async ({ input, context }) => {
+//     // your create code here
+//     return { id: 1, name: 'name' }
+//   })
+
+export const router = {
+  planet: {
+    list: listPlanet,
+    find: findPlanet,
+    // create: createPlanet
+  },
+};
